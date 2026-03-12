bc944afed1efc104044cfcbf5af8a3b27567fe38
Edwin Espinoza <edwin.smx2@gmail.com>
2026-03-12 12:00:31 +0100

    Creado controlador con endpoint GET "/"

---
+++ src/main/java/com/example/demo_thymeleaf/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo_thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("titulo", "Hola mundo!");
+        model.addAttribute("mensaje", "Bienvenido a Thymelead");
+        return "index";
+    }
+}
