d14a13aded06345ef3ba395e4b7a01593473f684
Edwin Espinoza <edwin.smx2@gmail.com>
2026-03-13 22:32:34 +0100

    Añadir plantilla resultat.html para devolver respuesta de enpoing POST /processar

--- src/main/java/com/example/demo_thymeleaf/controller/HomeController.java
+++ src/main/java/com/example/demo_thymeleaf/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.example.demo_thymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
